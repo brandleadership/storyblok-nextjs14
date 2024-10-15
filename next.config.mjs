633a9cb9e390d08ca897f0a3ccf58c213df8be85
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-10-15 14:42:10 +0200

    fix: remove doubleexport

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
+const nextConfig = {
+  eslint: {
+    // Disabling on production builds because we're running checks on PRs via GitHub Actions.
+    ignoreDuringBuilds: true,
+  },
+  pageExtensions: [
+    "page.tsx",
+    "page.js",
+    // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+    // Instead, use `not-found.ts` as a workaround
+    // "ts" is required to resolve `not-found.ts`
+    // https://github.com/vercel/next.js/issues/65447
+    "ts",
+  ],
 };
 
 export default nextConfig;
