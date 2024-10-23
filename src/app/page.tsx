e951928c7b2df40e2dd6ca117598b1243a0dc69d
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-10-23 20:43:48 +0200

    fature: draft and published mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
 
   return (
     <div>
-      <div>{process}</div>
+      <div>{JSON.stringify(process)}</div>
       {env}
       <ConfigHeader blok={header.content} />
       <StoryblokStory story={story} />
