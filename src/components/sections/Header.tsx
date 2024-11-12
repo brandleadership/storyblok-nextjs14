0f20245277761907147ac7757bca0a3c11b34a37
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-11-12 08:49:36 +0100

    feature: header styles

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,16 +1,17 @@
 import { storyblokEditable } from '@storyblok/react/rsc';
 
 const Header = ({ blok }: any) => (
-    <section {...storyblokEditable(blok)}>
-        <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
-            <img
-                src={blok.logo.filename}
-                className="h-6 w-6"
-                alt="Follow us on LinkedIn"
-            />
-            <p className="mb-8 text-lg font-normal text-gray-500 sm:px-16 lg:text-xl xl:px-48"></p>
-        </div>
-    </section>
+    <header {...storyblokEditable(blok)}>
+        <nav className="border-gray-200 bg-white px-4 py-2.5 lg:px-6 dark:bg-gray-800">
+            <div className="flex flex-wrap items-center justify-between">
+                <img
+                    src={blok.logo.filename}
+                    className="h-6 w-6"
+                    alt="Follow us on LinkedIn"
+                />
+            </div>
+        </nav>
+    </header>
 );
 
 export default Header;
