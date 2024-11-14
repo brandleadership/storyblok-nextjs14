847eb9d5b90741fb5990d73e9fee9eccd3db8f3f
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-11-14 15:27:21 +0100

    feature: footer/header with layout

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,16 +1,19 @@
 import { storyblokEditable } from '@storyblok/react/rsc';
 import { FooterStoryblok } from '../../../component-types-sb';
+import ContentWidth from '../layouts/ContentWidth';
 
 interface FooterProps {
     blok: FooterStoryblok;
 }
 
 const Footer = ({ blok }: FooterProps) => (
-    <footer {...storyblokEditable(blok)} className="px-4 py-2.5 lg:px-6">
-        <div className="text-left">
-            <p className="mb-8 text-lg font-normal">{blok.copyright}</p>
-        </div>
-    </footer>
+    <ContentWidth {...storyblokEditable(blok)}>
+        <footer className="px-4 py-2.5 lg:px-6">
+            <div className="text-left">
+                <p className="mb-8 text-lg font-normal">{blok.copyright}</p>
+            </div>
+        </footer>
+    </ContentWidth>
 );
 
 export default Footer;
